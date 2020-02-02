e5d68490afb2118ca867f0dc81d88ab85fcc262e
tjdghks1994 <parktjdghks@naver.com>
2020-02-02 18:00:34 +0900

    service 테스트 - 회원가입 관련(비밀번호 암호화 미적용)

---
+++ src/test/java/com/dailycooking/service/MemberRelationServiceTest.java
@@ -0,0 +1,38 @@
+package com.dailycooking.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.dailycooking.domain.MemberVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class MemberRelationServiceTest {
+
+	@Setter(onMethod_ = @Autowired)
+	private MemberRelationService service;
+
+	public void test() {
+		log.info(service);
+	}
+
+	@Test
+	public void testJoin() {
+		
+		MemberVO mvo = new MemberVO();
+		mvo.setUserid("test02");
+		mvo.setUsername("테스트 유저2");
+		mvo.setNickname("테스트유저2");
+		mvo.setUserpw("1111");
+		mvo.setUsermail("test2@naver.com");
+		
+		log.info("join 결과 : " +service.join(mvo));
+	}
+}
