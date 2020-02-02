fa3882693a3cc9ca666757e47a6f634cf1e4c9da
tjdghks1994 <parktjdghks@naver.com>
2020-02-02 16:20:57 +0900

    MemberVO 와 AuthVO 변수 설정

--- src/main/java/com/dailycooking/domain/AuthVO.java
+++ src/main/java/com/dailycooking/domain/AuthVO.java
@@ -1,5 +1,6 @@
 package com.dailycooking.domain;
 
 public class AuthVO {
-
+	private String userid; // 사용자 아이디
+	private String auth; // 사용자 권한
 }
--- src/main/java/com/dailycooking/domain/MemberVO.java
+++ src/main/java/com/dailycooking/domain/MemberVO.java
@@ -1,5 +1,14 @@
 package com.dailycooking.domain;
 
-public class MemberVO {
+import java.util.Date;
 
+public class MemberVO {
+	private String userid; // 사용자 아이디
+	private String username; // 사용자 이름
+	private String nickname; // 사용자 닉네임
+	private String usermail; // 사용자 이메일
+	private String userpw; // 사용자 비밀번호
+	
+	private Date regDate; // 사용자 가입 날짜
+	private int suspension; // 사용자 정지 여부 0 은 사용 가능한 상태 1 은 정지된 상태
 }
